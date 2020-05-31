3ed3b47aeb2fd266c67f3f85809fd09041132645
nicolas <nicolasCruzW21>
2020-05-31 12:48:43 -0400

    version 512x512 paper

--- models/networks.py
+++ models/networks.py
@@ -153,6 +153,8 @@ def define_G(input_nc, output_nc, ngf, netG, norm='batch', use_dropout=False, in
     elif netG == 'unet_128':
         net = UnetGenerator(input_nc, output_nc, 7, ngf, norm_layer=norm_layer, use_dropout=use_dropout)
     elif netG == 'unet_256':
+        net = UnetGenerator(input_nc, output_nc, 8, ngf, norm_layer=norm_layer,
+    elif netG == 'unet_512':
         net = UnetGenerator(input_nc, output_nc, 9, ngf, norm_layer=norm_layer, use_dropout=use_dropout)
     elif netG == 'cascade':
         net = cascaded_model(input_nc, output_nc , 256, ngf)
