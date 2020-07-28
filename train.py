ea7d40b75428a844ec97adb1d8313b1b389d0232
nicolas <nicolas.cruz@mundos-virtuales.com>
2020-07-28 01:35:41 -0400

    cleaning

--- train.py
+++ train.py
@@ -55,7 +55,6 @@ if __name__ == '__main__':
                 save_result = total_iters % opt.update_html_freq == 0
                 model.compute_visuals()
                 visualizer.display_current_results(model.get_current_visuals(), epoch, save_result)
-            print("total_iters",total_iters % opt.print_freq)
             if total_iters % opt.print_freq == 0:    # print training losses and save logging information to the disk
                 losses = model.get_current_losses()
                 t_comp = (time.time() - iter_start_time) / opt.batch_size
